4de8b6fa054f9f28d63985e1623a38e44314f576
DESKTOP-K9KH7O1\yxx <yanxiaoxu@dlydly.com>
2018-07-24 22:08:17 +0800

    Initial commit

---
+++ src/main/java/com/zy/wreserve/WreserveApplication.java
@@ -0,0 +1,12 @@
+package com.zy.wreserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WreserveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WreserveApplication.class, args);
+        }
+        }
